03db1c55d6677858ca7716732924dccb56191acf
aman chamoli <amanchamoli789@gmail.com>
2026-04-19 17:57:01 +0530

    "Add get,delete,pathapi"

--- sheryians/MogoDB_Notes/src/app.js
+++ sheryians/MogoDB_Notes/src/app.js
@@ -21,8 +21,39 @@ app.post("/notes",async (req,res) => {
 
 
 /*GET */
+
+app.get("/notes", async( req,res) => {
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"Notes fetched sucessfully",
+        data:notes
+    })
+})
+
 /*DELETE */
+
+app.delete("/notes/:id", async(req,res) =>{
+    const id = req.params.id
+
+    await noteModel.findOneAndDelete({_id:id})
+
+    res.status(200).json({
+        message:"Note deleted successfully",
+    })
+})
 /*PATCH */
+
+app.patch("/notes/:id", async(req,res) => {
+    const id = req.params.id
+    const description = req.body.description
+
+    await noteModel.findOneAndUpdate({_id:id},{description:description})
+
+    res.status(200).json({
+        message:"Note updated successfully",
+    })
+})
 
 module.exports = app;
 
--- sheryians/MogoDB_Notes/src/models/notes.model.js
+++ sheryians/MogoDB_Notes/src/models/notes.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema({
     title: String,
-    content: String,
+    description: String,
     type :String,
 })
 
